31462b868b34c6e2f367f1d146318da095bd76fb
Saeid Babaei <babaei503@gmail.com>
2019-12-30 14:42:41 +0100

    Using Eureka Naming Server

---
+++ eureka-service/src/test/java/ir/saeidbabaei/microservice/eureka/naming/server/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.saeidbabaei.microservice.eureka.naming.server.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
